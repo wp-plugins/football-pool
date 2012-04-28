d9c0e63b839599e2eaa38e75d2df32b0423613c1
AntoineH <AntoineH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-28 21:03:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/football-pool/branches/1.2@537722 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/widget-football-pool-group.php
+++ widgets/widget-football-pool-group.php
@@ -94,10 +94,17 @@ class Football_Pool_Group_Widget extends WP_Widget {
 			),
 			array(
 				'name'    => 'Show this group',
-				'desc' => '',
+				'desc'    => '',
 				'id'      => 'group',
 				'type'    => 'select',
 				'options' => array() // get data from the database later on (function form)
+			),
+			array(
+				'name'    => 'Layout of the widget',
+				'desc'    => '',
+				'id'      => 'layout',
+				'type'    => 'select',
+				'options' => array() // get data later on (function form)
 			),
 		)
 	);
@@ -117,13 +124,15 @@ class Football_Pool_Group_Widget extends WP_Widget {
 		extract( $args );
 		
 		$group = ! empty( $instance['group'] ) ? $instance['group'] : '1';
+		$layouts = array( 1 => 'wide', 2 => 'small' );
+		$layout = $layouts[ $instance['layout'] ];
 		
 		if ( $title != '' ) {
 			echo $before_title . $title . $after_title;
 		}
 		
 		$groups = new Football_Pool_Groups;
-		echo $groups->print_group_standing( $group );
+		echo $groups->print_group_standing( $group, $layout );
 	}
 	
 	/**
@@ -194,6 +203,12 @@ class Football_Pool_Group_Widget extends WP_Widget {
 			$options[ $id ] = $group;
 		}
 		$this->widget['fields'][1]['options'] = $options;
+		
+		// set the layout options
+		$this->widget['fields'][2]['options'] = array ( 
+														1 => __( 'breed', FOOTBALLPOOL_TEXT_DOMAIN ), 
+														2 => __( 'smal', FOOTBALLPOOL_TEXT_DOMAIN ) 
+												);
 		
 		$defaults = array(
 			'id' => '',
