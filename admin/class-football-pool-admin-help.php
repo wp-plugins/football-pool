e035d714bb1b503ab97015e1b4388ac991ddaeab
AntoineH <AntoineH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-16 07:41:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/football-pool/branches/1.3@544842 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/class-football-pool-admin-help.php
+++ admin/class-football-pool-admin-help.php
@@ -5,7 +5,17 @@ class Football_Pool_Admin_Help extends Football_Pool_Admin {
 	public function admin() {
 		self::admin_header( __( 'Help', FOOTBALLPOOL_TEXT_DOMAIN ), '' );
 		?>
-		<h2>Shortcodes</h2>
+		<h2>Index</h2>
+		<p>
+			<ol>
+				<li><a href="#shortcodes">Shortcodes</a></li>
+				<li><a href="#leagues">Leagues</a></li>
+				<li><a href="#players">Players</a></li>
+				<li><a href="#bonusquestions">Bonus questions</a></li>
+			</ol>
+		</p>
+		
+		<h2 id="shortcodes">Shortcodes</h2>
 		<h3>[fp-groups]</h3>
 		<p>Shows a group standing for the group stage of the tournament. Parameter "id" must be given. If "id" is 
 		ommited, or not a valid group id, then nothing will be returned.</p>
@@ -22,7 +32,7 @@ class Football_Pool_Admin_Help extends Football_Pool_Admin {
 		</p>
 		<p>example:<br />
 		<span class="code">[fp-groups id=2]</span><br />
-		<img src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-shortcode-groups.png" alt="screenshot" />
+		<img class="screenshot" src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-shortcode-groups.png" alt="screenshot" />
 		</p>
 		
 		<h3>[fp-ranking]</h3>
@@ -52,7 +62,7 @@ class Football_Pool_Admin_Help extends Football_Pool_Admin {
 		</p>
 		<p>example:<br />
 		<span class="code">[fp-ranking num=5 date="2012-06-22 11:00"]</span><br />
-		<img src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-shortcode-ranking.png" alt="screenshot" />
+		<img class="screenshot" src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-shortcode-ranking.png" alt="screenshot" />
 		</p>
 
 		<h3>[fp-register]link text[/fp-register]</h3>
@@ -79,13 +89,35 @@ class Football_Pool_Admin_Help extends Football_Pool_Admin {
 		<p>See <a href="?page=footballpool-options">Football Pool plugin settings</a> for some basic shortcodes that  will display the value for a plugin setting.</p>
 		<p>
 
-		<h2>Leagues</h2>
+		<h2 id="leagues">Leagues</h2>
 		<p>The plugin supports placing players in different leagues. For example when you want to group players per department, or friends and family, or paying and non-paying, etc. When playing with leagues an admin has to 'approve' the league for which a player subscribed. That can be done in the <a href="users.php">User</a> section of the WordPress admin, or the <a href="?page=footballpool-users">Users page</a> of the Football Pool plugin.</p>
 		<p>If using leagues all players have to be a member of a league, otherwise they are not considered to be a football pool player.</p>
 
-		<h2>Players</h2>
+		<h2 id="players">Players</h2>
 		<p>There's two ways the plugin can handle your blog users: via leagues or not via leagues. If playing with leagues your blog users have to be added to an active league. New subscribers to your blog must choose a league when subscribing, but existing users have to change this setting after the plugin is installed (or the admin can do this for them).<br />
 		If not playing with leagues all your blog users are automatically players in the pool. If you want to exclude some players from the rankings (e.g. the admin), you can disable them in the <a href="?page=footballpool-users">Users page</a> of the Football Pool plugin.</p>
+		
+		<h2 id="bonusquestions">Bonus questions</h2>
+		<h3>Types</h3>
+		<p>There are 3 types of bonus questions: 
+		<ol>
+			<li>Text questions</li>
+			<li>Multiple choice questions (one answer)</li>
+			<li>Multiple choice questions (one or more answers)</li>
+		</ol>
+		Each question type can also show an (optional) image.
+		</p>
+		<p>For multiple choice questions you have to give 2 or more options to the players. The possible answers must be entered as a semicolon separated list.</p>
+		<h3>Giving points</h3>
+		<p>After the 'answer before' date has passed and your players may not alter their answers, an admin has to manually approve all answers for a question. For this, go to the <a href="?page=footballpool-bonus">Questions admin screen</a> and click on the "User Answers" link.<br />
+		<img class="screenshot" src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-bonus-user-answers-1.png" alt="screenshot" />
+		</p>
+		<p>In the answer screen information about the question is shown as a reference (1). The answer and default points are shown and - if an admin has filled in the answer - the answer is also shown.<br />
+		For each player click the appropiate radiobutton for a right or wrong answer (2). If an answer is considered right you have the possibility to give a different amount of points to that user (3). For example to give extra bonuspoints or to give half the points for an incomplete answer. Leave blank if you want to give the default points for that question.<br />
+		<img class="screenshot" src="<?php echo FOOTBALLPOOL_PLUGIN_URL; ?>admin/assets/example-bonus-user-answers-2.png" alt="screenshot" />
+		</p>
+		<p>Please note that for points for bonus questions to be added to the total points for a player an admin also has to fill in the score date field for that question. The score date is used to determine the order in which points are plotted in the charts.</p>
+		
 		<?php
 		self::admin_footer();
 	}
